4ce3f7d38b72ef1b9bf96e9d53da109d0ebdd2c7
Martin Grani <martingrani@gmail.com>
2026-01-01 18:47:16 +0100

    Refactor i2c_send function

--- Core/Src/i2c.c
+++ Core/Src/i2c.c
@@ -2,7 +2,7 @@
 #include "stm32f042x6.h"
 #include <main.h>
 #include <stdint.h>
-#include <sys/types.h>
+// #include <sys/types.h>
 
 // Init PB7 & PB6 for I2C
 void i2c_init() {
@@ -42,39 +42,45 @@ void i2c_init() {
   I2C1->CR1 |= I2C_CR1_PE;
 }
 
-void clear_flags() { I2C1->ICR = I2C_ICR_STOPCF | I2C_ICR_NACKCF; }
+void clear_flags() {
+  // Clear command flags that may hinder transfer\reiceive
+  I2C1->ICR = I2C_ICR_STOPCF | I2C_ICR_NACKCF; 
+}
 
-void i2c_send(uint8_t address, uint8_t data[], uint8_t length)
-{
-    while (I2C1->ISR & I2C_ISR_BUSY) { }
+void i2c_send(uint8_t address, uint8_t data[], uint8_t length) {
+  // Wait until bus is not busy
+  while (I2C1->ISR & I2C_ISR_BUSY); 
 
-    clear_flags(); // bør skrive til ICR, ikke ISR
+  // Clear STOP and NACK flags.
+  clear_flags(); 
 
-    // Clear felter vi setter (felt, ikke *_Pos)
-    I2C1->CR2 &= ~(I2C_CR2_SADD | I2C_CR2_NBYTES | I2C_CR2_RD_WRN |
-                  I2C_CR2_RELOAD | I2C_CR2_AUTOEND);
+  // Clear fields to be sure we write to empty fields 
+  I2C1->CR2 &= ~(I2C_CR2_SADD | I2C_CR2_NBYTES | I2C_CR2_RD_WRN |
+                 I2C_CR2_RELOAD | I2C_CR2_AUTOEND);
 
-    // Sett adresse + nbytes + write + autoend
-    // NB: vurder (address << 1) hvis du ikke får ACK (STM32F0-adresseplassering)
-    I2C1->CR2 |= ((uint32_t)(address << 1) << I2C_CR2_SADD_Pos);
-    I2C1->CR2 |= ((uint32_t)length << I2C_CR2_NBYTES_Pos);
-    I2C1->CR2 |= I2C_CR2_AUTOEND;     // stop automatisk etter NBYTES
+  // Configure transfer address, NBYTES, write and enable AUTOEND 
+  I2C1->CR2 |= ((uint32_t)(address << 1) << I2C_CR2_SADD_Pos);
+  I2C1->CR2 |= ((uint32_t)length << I2C_CR2_NBYTES_Pos);
+  I2C1->CR2 |= I2C_CR2_AUTOEND; 
 
-    // Start
-    I2C1->CR2 |= I2C_CR2_START;
+  // Start
+  I2C1->CR2 |= I2C_CR2_START;
 
-    for (uint8_t i = 0; i < length; i++) {
+  // End if NACK received from slave. Check if TXDR is empty (previous data is sent). Place data in TXDR register.
+  for (uint8_t i = 0; i < length; i++) {
 
-        while (!(I2C1->ISR & I2C_ISR_TXIS)) {
-            if (I2C1->ISR & I2C_ISR_NACKF) {
-                I2C1->ICR = I2C_ICR_NACKCF;
-                return;
-            }
-        }
-
-        I2C1->TXDR = data[i];
+    while (!(I2C1->ISR & I2C_ISR_TXIS)) {
+      if (I2C1->ISR & I2C_ISR_NACKF) {
+        I2C1->ICR = I2C_ICR_NACKCF;
+        return;
+      }
     }
 
-    while (!(I2C1->ISR & I2C_ISR_STOPF)) { }
-    I2C1->ICR = I2C_ICR_STOPCF;
+    I2C1->TXDR = data[i];
+  }
+
+  // Check if STOP is sent, clear STOP flag for next use of I2C1
+  while (!(I2C1->ISR & I2C_ISR_STOPF)) {
+  }
+  I2C1->ICR = I2C_ICR_STOPCF;
 }
